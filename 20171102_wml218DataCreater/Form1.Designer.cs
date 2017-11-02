4d3d4008825f9fb483dca659ff928e582d5d5650
汤圆 <chenxinyuan0842@126.com>
2017-11-02 15:19:25 +0800

    Finish

--- 20171102_wml218DataCreater/Form1.Designer.cs
+++ 20171102_wml218DataCreater/Form1.Designer.cs
@@ -28,26 +28,297 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lTopic1 = new System.Windows.Forms.Label();
+            this.lTopic2 = new System.Windows.Forms.Label();
+            this.lTopic3 = new System.Windows.Forms.Label();
+            this.lName1 = new System.Windows.Forms.Label();
+            this.gBox1 = new System.Windows.Forms.GroupBox();
+            this.lTotalNum2 = new System.Windows.Forms.Label();
+            this.tTotalNum = new System.Windows.Forms.TextBox();
+            this.lTotalNum1 = new System.Windows.Forms.Label();
+            this.lIdentity2 = new System.Windows.Forms.Label();
+            this.cBoxIdentity = new System.Windows.Forms.ComboBox();
+            this.lIdentity1 = new System.Windows.Forms.Label();
+            this.lInfo2 = new System.Windows.Forms.Label();
+            this.lName2 = new System.Windows.Forms.Label();
+            this.tInfo = new System.Windows.Forms.TextBox();
+            this.lInfo1 = new System.Windows.Forms.Label();
+            this.tName = new System.Windows.Forms.TextBox();
+            this.saveFileDialog = new System.Windows.Forms.SaveFileDialog();
+            this.bCheck = new System.Windows.Forms.Button();
+            this.tPaste = new System.Windows.Forms.TextBox();
+            this.lPaste = new System.Windows.Forms.Label();
+            this.bSave = new System.Windows.Forms.Button();
+            this.bConfirm = new System.Windows.Forms.Button();
+            this.bReset = new System.Windows.Forms.Button();
+            this.gBox1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // textBox1
+            // lTopic1
             // 
-            this.textBox1.Location = new System.Drawing.Point(50, 12);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox1.Size = new System.Drawing.Size(173, 237);
-            this.textBox1.TabIndex = 0;
+            this.lTopic1.AutoSize = true;
+            this.lTopic1.Font = new System.Drawing.Font("微软雅黑", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lTopic1.Location = new System.Drawing.Point(12, 9);
+            this.lTopic1.Name = "lTopic1";
+            this.lTopic1.Size = new System.Drawing.Size(288, 27);
+            this.lTopic1.TabIndex = 0;
+            this.lTopic1.Text = "为民楼218活动室自助预约平台";
+            // 
+            // lTopic2
+            // 
+            this.lTopic2.AutoSize = true;
+            this.lTopic2.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lTopic2.Location = new System.Drawing.Point(12, 36);
+            this.lTopic2.Name = "lTopic2";
+            this.lTopic2.Size = new System.Drawing.Size(201, 28);
+            this.lTopic2.TabIndex = 1;
+            this.lTopic2.Text = "用户数据导入编辑器";
+            // 
+            // lTopic3
+            // 
+            this.lTopic3.AutoSize = true;
+            this.lTopic3.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lTopic3.Location = new System.Drawing.Point(219, 42);
+            this.lTopic3.Name = "lTopic3";
+            this.lTopic3.Size = new System.Drawing.Size(69, 20);
+            this.lTopic3.TabIndex = 2;
+            this.lTopic3.Text = "v1.1 Beta";
+            // 
+            // lName1
+            // 
+            this.lName1.AutoSize = true;
+            this.lName1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lName1.Location = new System.Drawing.Point(15, 28);
+            this.lName1.Name = "lName1";
+            this.lName1.Size = new System.Drawing.Size(249, 17);
+            this.lName1.TabIndex = 3;
+            this.lName1.Text = "输出文件名                                         .txt";
+            // 
+            // gBox1
+            // 
+            this.gBox1.Controls.Add(this.lTotalNum2);
+            this.gBox1.Controls.Add(this.tTotalNum);
+            this.gBox1.Controls.Add(this.lTotalNum1);
+            this.gBox1.Controls.Add(this.lIdentity2);
+            this.gBox1.Controls.Add(this.cBoxIdentity);
+            this.gBox1.Controls.Add(this.lIdentity1);
+            this.gBox1.Controls.Add(this.lInfo2);
+            this.gBox1.Controls.Add(this.lName2);
+            this.gBox1.Controls.Add(this.tInfo);
+            this.gBox1.Controls.Add(this.lInfo1);
+            this.gBox1.Controls.Add(this.tName);
+            this.gBox1.Controls.Add(this.lName1);
+            this.gBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.gBox1.Location = new System.Drawing.Point(17, 77);
+            this.gBox1.Name = "gBox1";
+            this.gBox1.Size = new System.Drawing.Size(288, 221);
+            this.gBox1.TabIndex = 4;
+            this.gBox1.TabStop = false;
+            this.gBox1.Text = " 数据文件设置项 ";
+            // 
+            // lTotalNum2
+            // 
+            this.lTotalNum2.AutoSize = true;
+            this.lTotalNum2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lTotalNum2.Location = new System.Drawing.Point(58, 191);
+            this.lTotalNum2.Name = "lTotalNum2";
+            this.lTotalNum2.Size = new System.Drawing.Size(164, 17);
+            this.lTotalNum2.TabIndex = 14;
+            this.lTotalNum2.Text = "注：数据检查完成后自动生成";
+            // 
+            // tTotalNum
+            // 
+            this.tTotalNum.Enabled = false;
+            this.tTotalNum.Location = new System.Drawing.Point(88, 165);
+            this.tTotalNum.Name = "tTotalNum";
+            this.tTotalNum.Size = new System.Drawing.Size(149, 23);
+            this.tTotalNum.TabIndex = 13;
+            this.tTotalNum.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // lTotalNum1
+            // 
+            this.lTotalNum1.AutoSize = true;
+            this.lTotalNum1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lTotalNum1.Location = new System.Drawing.Point(26, 168);
+            this.lTotalNum1.Name = "lTotalNum1";
+            this.lTotalNum1.Size = new System.Drawing.Size(56, 17);
+            this.lTotalNum1.TabIndex = 12;
+            this.lTotalNum1.Text = "数据条数";
+            // 
+            // lIdentity2
+            // 
+            this.lIdentity2.AutoSize = true;
+            this.lIdentity2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lIdentity2.Location = new System.Drawing.Point(58, 145);
+            this.lIdentity2.Name = "lIdentity2";
+            this.lIdentity2.Size = new System.Drawing.Size(212, 17);
+            this.lIdentity2.TabIndex = 11;
+            this.lIdentity2.Text = "注：学硕、专硕请统一选择“硕士生”";
+            // 
+            // cBoxIdentity
+            // 
+            this.cBoxIdentity.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cBoxIdentity.FormattingEnabled = true;
+            this.cBoxIdentity.Items.AddRange(new object[] {
+            "1. 本科生",
+            "2. 硕士生",
+            "3. 博士生",
+            "4. 辅导员",
+            "5. 教职工"});
+            this.cBoxIdentity.Location = new System.Drawing.Point(88, 117);
+            this.cBoxIdentity.Name = "cBoxIdentity";
+            this.cBoxIdentity.Size = new System.Drawing.Size(149, 25);
+            this.cBoxIdentity.TabIndex = 10;
+            // 
+            // lIdentity1
+            // 
+            this.lIdentity1.AutoSize = true;
+            this.lIdentity1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lIdentity1.Location = new System.Drawing.Point(26, 120);
+            this.lIdentity1.Name = "lIdentity1";
+            this.lIdentity1.Size = new System.Drawing.Size(56, 17);
+            this.lIdentity1.TabIndex = 9;
+            this.lIdentity1.Text = "人员类别";
+            // 
+            // lInfo2
+            // 
+            this.lInfo2.AutoSize = true;
+            this.lInfo2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lInfo2.Location = new System.Drawing.Point(58, 97);
+            this.lInfo2.Name = "lInfo2";
+            this.lInfo2.Size = new System.Drawing.Size(222, 17);
+            this.lInfo2.TabIndex = 8;
+            this.lInfo2.Text = "例：2017级本科 / 2018级硕士 / 辅导员";
+            // 
+            // lName2
+            // 
+            this.lName2.AutoSize = true;
+            this.lName2.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lName2.Location = new System.Drawing.Point(58, 51);
+            this.lName2.Name = "lName2";
+            this.lName2.Size = new System.Drawing.Size(166, 17);
+            this.lName2.TabIndex = 7;
+            this.lName2.Text = "例：BK2017 / SY2018 / FDY";
+            // 
+            // tInfo
+            // 
+            this.tInfo.Location = new System.Drawing.Point(88, 71);
+            this.tInfo.Name = "tInfo";
+            this.tInfo.Size = new System.Drawing.Size(149, 23);
+            this.tInfo.TabIndex = 6;
+            this.tInfo.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tInfo.Click += new System.EventHandler(this.tInfo_Click);
+            this.tInfo.TextChanged += new System.EventHandler(this.tInfo_TextChanged);
+            // 
+            // lInfo1
+            // 
+            this.lInfo1.AutoSize = true;
+            this.lInfo1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lInfo1.Location = new System.Drawing.Point(26, 74);
+            this.lInfo1.Name = "lInfo1";
+            this.lInfo1.Size = new System.Drawing.Size(56, 17);
+            this.lInfo1.TabIndex = 5;
+            this.lInfo1.Text = "数据内容";
+            // 
+            // tName
+            // 
+            this.tName.Location = new System.Drawing.Point(88, 25);
+            this.tName.Name = "tName";
+            this.tName.Size = new System.Drawing.Size(149, 23);
+            this.tName.TabIndex = 4;
+            this.tName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tName.Click += new System.EventHandler(this.tName_Click);
+            // 
+            // bCheck
+            // 
+            this.bCheck.Enabled = false;
+            this.bCheck.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bCheck.Location = new System.Drawing.Point(399, 239);
+            this.bCheck.Name = "bCheck";
+            this.bCheck.Size = new System.Drawing.Size(87, 28);
+            this.bCheck.TabIndex = 5;
+            this.bCheck.Text = "检查并处理";
+            this.bCheck.UseVisualStyleBackColor = true;
+            this.bCheck.Click += new System.EventHandler(this.bCheck_Click);
+            // 
+            // tPaste
+            // 
+            this.tPaste.Enabled = false;
+            this.tPaste.Font = new System.Drawing.Font("微软雅黑", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.tPaste.Location = new System.Drawing.Point(319, 29);
+            this.tPaste.Multiline = true;
+            this.tPaste.Name = "tPaste";
+            this.tPaste.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.tPaste.Size = new System.Drawing.Size(167, 204);
+            this.tPaste.TabIndex = 6;
+            // 
+            // lPaste
+            // 
+            this.lPaste.AutoSize = true;
+            this.lPaste.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lPaste.Location = new System.Drawing.Point(316, 9);
+            this.lPaste.Name = "lPaste";
+            this.lPaste.Size = new System.Drawing.Size(133, 17);
+            this.lPaste.TabIndex = 15;
+            this.lPaste.Text = "请在此粘贴Excel表内容";
+            // 
+            // bSave
+            // 
+            this.bSave.Enabled = false;
+            this.bSave.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bSave.Location = new System.Drawing.Point(399, 270);
+            this.bSave.Name = "bSave";
+            this.bSave.Size = new System.Drawing.Size(87, 28);
+            this.bSave.TabIndex = 16;
+            this.bSave.Text = "保存文件";
+            this.bSave.UseVisualStyleBackColor = true;
+            this.bSave.Click += new System.EventHandler(this.bSave_Click);
+            // 
+            // bConfirm
+            // 
+            this.bConfirm.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bConfirm.Location = new System.Drawing.Point(319, 239);
+            this.bConfirm.Name = "bConfirm";
+            this.bConfirm.Size = new System.Drawing.Size(74, 28);
+            this.bConfirm.TabIndex = 17;
+            this.bConfirm.Text = "确认设置";
+            this.bConfirm.UseVisualStyleBackColor = true;
+            this.bConfirm.Click += new System.EventHandler(this.bConfirm_Click);
+            // 
+            // bReset
+            // 
+            this.bReset.Enabled = false;
+            this.bReset.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.bReset.Location = new System.Drawing.Point(319, 270);
+            this.bReset.Name = "bReset";
+            this.bReset.Size = new System.Drawing.Size(74, 28);
+            this.bReset.TabIndex = 18;
+            this.bReset.Text = "重新粘贴";
+            this.bReset.UseVisualStyleBackColor = true;
+            this.bReset.Click += new System.EventHandler(this.bReset_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.textBox1);
+            this.ClientSize = new System.Drawing.Size(498, 312);
+            this.Controls.Add(this.bReset);
+            this.Controls.Add(this.bConfirm);
+            this.Controls.Add(this.bSave);
+            this.Controls.Add(this.lPaste);
+            this.Controls.Add(this.tPaste);
+            this.Controls.Add(this.bCheck);
+            this.Controls.Add(this.gBox1);
+            this.Controls.Add(this.lTopic3);
+            this.Controls.Add(this.lTopic2);
+            this.Controls.Add(this.lTopic1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "用户数据导入生成器";
+            this.gBox1.ResumeLayout(false);
+            this.gBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -55,7 +326,29 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lTopic1;
+        private System.Windows.Forms.Label lTopic2;
+        private System.Windows.Forms.Label lTopic3;
+        private System.Windows.Forms.Label lName1;
+        private System.Windows.Forms.GroupBox gBox1;
+        private System.Windows.Forms.TextBox tInfo;
+        private System.Windows.Forms.Label lInfo1;
+        private System.Windows.Forms.TextBox tName;
+        private System.Windows.Forms.Label lName2;
+        private System.Windows.Forms.Label lInfo2;
+        private System.Windows.Forms.Label lIdentity2;
+        private System.Windows.Forms.ComboBox cBoxIdentity;
+        private System.Windows.Forms.Label lIdentity1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog;
+        private System.Windows.Forms.Label lTotalNum2;
+        private System.Windows.Forms.TextBox tTotalNum;
+        private System.Windows.Forms.Label lTotalNum1;
+        private System.Windows.Forms.Button bCheck;
+        private System.Windows.Forms.TextBox tPaste;
+        private System.Windows.Forms.Label lPaste;
+        private System.Windows.Forms.Button bSave;
+        private System.Windows.Forms.Button bConfirm;
+        private System.Windows.Forms.Button bReset;
     }
 }
 
--- 20171102_wml218DataCreater/Form1.cs
+++ 20171102_wml218DataCreater/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
@@ -14,6 +15,133 @@ namespace _20171102_wml218DataCreater
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void bCheck_Click(object sender, EventArgs e)
+        {
+            string[] strLine = tPaste.Lines;
+            int totalLines = strLine.Length;
+            string[] userid = new string[totalLines];
+            string[] username = new string[totalLines];
+            int js = 0;
+            for (int i = 0; i < totalLines; i++)
+            {
+                if (strLine[i] != "")
+                {
+                    js++;
+                    string tmpStr = strLine[i].Replace("\t","`");
+                    userid[js] = tmpStr.Substring(0, tmpStr.IndexOf('`'));
+                    username[js] = tmpStr.Substring(tmpStr.IndexOf('`')).Replace("`", "");
+                }
+            }
+            totalLines = js;
+            tTotalNum.Text = totalLines.ToString();
+
+            string identityStr = "" ;
+            switch (cBoxIdentity.SelectedIndex)
+            {
+                case 0:
+                    identityStr = "bk";
+                    break;
+                case 1:
+                    identityStr = "szy";
+                    break;
+                case 2:
+                    identityStr = "by";
+                    break;
+                case 3:
+                    identityStr = "fdy";
+                    break;
+                case 4:
+                    identityStr = "jg";
+                    break;
+            }
+
+            tPaste.Text = "";
+            tPaste.Text += "[Name]" + tName.Text.Trim() + Environment.NewLine;
+            tPaste.Text += "[TotalRecord]" + totalLines.ToString() + Environment.NewLine;
+            tPaste.Text += "[Identity]" + identityStr + Environment.NewLine;
+            for (int i = 1; i <= totalLines; i++)
+                tPaste.Text += userid[i] + "," + username[i] + Environment.NewLine;
+            tPaste.Text += "[EOF.]";
+            tPaste.ReadOnly = true;
+            bConfirm.Enabled = false;
+            bReset.Enabled = true;
+            bSave.Enabled = true;
+            bCheck.Enabled = false;
+
+            bSave.Focus();
+        }
+
+        private void bSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Title = "请选择数据文件保存位置";
+            saveFileDialog.Filter = "文本文件(*.txt)|";
+            saveFileDialog.FileName = tName.Text + ".txt";
+            saveFileDialog.AddExtension = true;
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                var utf8WithoutBom = new UTF8Encoding(false);
+                using (var sink = new StreamWriter(saveFileDialog.FileName.ToString(), false, utf8WithoutBom))
+                {
+                    sink.Write(tPaste.Text);
+                }
+            }
+        }
+
+        private void tName_Click(object sender, EventArgs e)
+        {
+            tName.SelectAll();
+        }
+
+        private void tInfo_Click(object sender, EventArgs e)
+        {
+            tInfo.SelectAll();
+        }
+
+        private void bConfirm_Click(object sender, EventArgs e)
+        {
+            tName.Text = tName.Text.Trim();
+            tInfo.Text = tInfo.Text.Trim();
+            if (tName.Text != "" && tInfo.Text != "" && cBoxIdentity.SelectedIndex != -1)
+            {
+                gBox1.Enabled = false;
+                tPaste.Enabled = true;
+                bCheck.Enabled = true;
+                bConfirm.Enabled = false;
+            }
+            else
+                MessageBox.Show("数据文件设置未完成！");
+        }
+
+        private void tInfo_TextChanged(object sender, EventArgs e)
+        {
+            if (tInfo.Text.IndexOf("本") >= 0)
+                cBoxIdentity.SelectedIndex = 0;
+            else if (tInfo.Text.IndexOf("硕") >= 0)
+                cBoxIdentity.SelectedIndex = 1;
+            else if (tInfo.Text.IndexOf("博") >= 0)
+                cBoxIdentity.SelectedIndex = 2;
+            else if (tInfo.Text.IndexOf("辅") >= 0)
+                cBoxIdentity.SelectedIndex = 3;
+            else if (tInfo.Text.IndexOf("教") >= 0)
+                cBoxIdentity.SelectedIndex = 4;
+            else
+                cBoxIdentity.SelectedIndex = -1;
+        }
+
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            gBox1.Enabled = true;
+            tPaste.ReadOnly = false;
+            tPaste.Text = "";
+            tPaste.Enabled = false;
+            bCheck.Enabled = false;
+            bSave.Enabled = false;
+            bConfirm.Enabled = true;
+
         }
     }
 }
